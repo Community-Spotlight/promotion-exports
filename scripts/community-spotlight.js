55f374079eb92f90eedbeceb7dc5dcdd269400a7
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2024-07-16 22:57:47 -0700

    Update community-spotlight.js

--- scripts/community-spotlight.js
+++ scripts/community-spotlight.js
@@ -1,1 +1,29 @@
-// WIP
+// WIP DO NOT USE YET!
+/*
+  -= Community Spotlight =-
+  By: SharkPool and the Community Spotlight Team
+  Developers may Freely use this to Incorporate Promotions in their Projects
+
+  Licence: MIT
+  Version: 0.0.01
+*/
+
+// CS Storage
+window.CSPromos = [];
+
+// Fetches and Saves all Promotions to the Window
+// Run this Function before using any other Function
+async function refreshPromos() {
+  
+}
+
+// Returns a random Promotion based on given Parameters. Returns a JSON with Info and the URL to the Promotion
+/*
+  type: "image" or "video"
+  optParams: {
+    
+  }
+*/
+async function getPromotion(type, optParams) {
+  
+}
